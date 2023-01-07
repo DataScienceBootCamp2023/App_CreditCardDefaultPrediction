d6efe80fdef8ab3d2557d6b0610b2a36a7836d26
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-07 17:18:15 +0300

    Update and rename app.py to flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,59 @@
+###############################################################################
+#                            RUN MAIN                                         #
+###############################################################################
+
+# setup
+## pkg
+import flask
+import pandas as pd
+
+from models.credit_card_default_prediction import CreditCardDefaultPrediction
+from settings import config
+
+## app
+app = flask.Flask(__name__, 
+				instance_relative_config=True, 
+       			template_folder=config.root+'client/templates',
+                static_folder=config.root+'client/static')
+
+
+
+# main
+@app.route("/", methods=['GET','POST'])
+def index():
+	try:
+		if flask.request.method == 'POST':
+			## data from client
+			app.logger.info(flask.request.files)
+			app.logger.info(flask.request.form)
+			dtf_input = pd.read_excel(flask.request.files["dtf_input"])
+			app.logger.warning("--- Inputs Received ---")
+
+			## Predict
+			model = CreditCardDefaultPrediction(dtf_input)
+
+			return flask.send_file(attachment_filename='CreditCardDefaultPrediction.xlsx', as_attachment=True)             
+		else:
+			return flask.render_template("index.html")
+
+	except Exception as e:
+		app.logger.error(e)
+		flask.abort(500)
+    
+
+   
+# errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return flask.render_template("errors.html", msg="Page doesn't exist"), 404
+    
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return flask.render_template('errors.html', msg="Something went terribly wrong"), 500
+
+
+
+# run
+if __name__ == "__main__":
+    app.run(host=config.host, port=config.port, debug=config.debug)
